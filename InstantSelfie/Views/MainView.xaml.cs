f8504520b37778bde24d614fdcbf94f9dc28ab8c
Matthew Wallace <matthew.wallace@riotgames.com>
2017-02-14 07:12:27 -0600

    instant selfie - 01 setting up the project

---
+++ InstantSelfie/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InstantSelfie
+{
+	public partial class MainView : ContentPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
